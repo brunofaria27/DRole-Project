a1bbddcdc7ed3f77aa305fca9b6d56f4979082d7
Guilherme Dantas <guilherme.dantas1508@gmail.com>
2021-10-25 21:34:18 -0300

    Adição de teste para DAO e adição do DAO base

--- drole-backend/src/main/java/app/ApplicationTest.java
+++ drole-backend/src/main/java/app/ApplicationTest.java
@@ -2,9 +2,15 @@ package app;
 
 import static spark.Spark.*;
 
+import dao.DAO;
+
 public class ApplicationTest {
 	public static void main(String[] args) {
 		port(4568);
 		get("/hello", (request, response) -> "Hello World! Teste para todos os PCs");
+		
+		DAO dao = new DAO();
+		dao.connect();
+		dao.close();
 	}
 }
