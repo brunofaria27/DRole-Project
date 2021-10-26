cb8811dde4f106271be2c4e8c8b65cc5738f7a84
mit018 <lauraxavier018@gmail.com>
2021-10-26 17:09:34 -0300

    DAO, Service - Events

--- drole-backend/src/main/java/app/Application.java
+++ drole-backend/src/main/java/app/Application.java
@@ -4,17 +4,16 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import model.Events;
 import model.Score;
-import model.Users;
+import model.User;
 
 public class Application {
 	private static Calendar calendario = Calendar.getInstance();
 	private static Events eventTest = new Events(1, "Baile do teste", 3, "Pop", 18, 5, "Pendente", "2022-11-11 00:00:00");
 	private static Score scoreTest = new Score(1, 4, calendario.getTime(), 3);
-	private static Users userTest = new Users("mit", 1, "foto", "mit@gmail.com", "asfgsdfh",
+	private static User userTest = new User("mit", 1, "foto", "mit@gmail.com", "asfgsdfh",
 			"bh", "linda", "Laura Xavier");
 	
 	public static void main(String[] args) {
