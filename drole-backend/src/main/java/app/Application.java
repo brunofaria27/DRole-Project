bf91275f60ead4335159e8938c9e9fa01a803075
mit018 <lauraxavier018@gmail.com>
2021-10-26 17:39:28 -0300

    teste eventos

--- drole-backend/src/main/java/app/Application.java
+++ drole-backend/src/main/java/app/Application.java
@@ -1,28 +1,33 @@
 package app;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-
-import java.util.Calendar;
+import static spark.Spark.*;
 
 import model.Events;
-import model.Score;
-import model.User;
+import services.LoginService;
+import services.UserService;
+import services.EventsService;
 
 public class Application {
-	private static Calendar calendario = Calendar.getInstance();
-	private static Events eventTest = new Events(1, "Baile do teste", 3, "Pop", 18, 5, "Pendente", "2022-11-11 00:00:00");
-	private static Score scoreTest = new Score(1, 4, calendario.getTime(), 3);
-	private static User userTest = new User("mit", 1, "foto", "mit@gmail.com", "asfgsdfh",
-			"bh", "linda", "Laura Xavier");
-	
+	//private static Events eventTest = new Events(1, "Baile do teste", 3, "Pop", 18, 5, "Pendente", "2022-11-11 00:00:00");
+	private static UserService userService = new UserService();
+	public static LoginService loginService = new LoginService();
+	public static EventsService eventService = new EventsService();
 	public static void main(String[] args) {
 		port(4568);
-		get("/eventos", (request, response) -> eventTest.toString());
-		get("/score", (request, response) -> scoreTest.toString());
-		get("/user", (request, response) -> userTest.toString());
-		System.out.println(eventTest.toString());
-		System.out.println(scoreTest.toString());
-		System.out.println(userTest.toString());
+		
+		
+		post("/user/", (request, response) -> userService.add(request, response));
+		post("/user/", (request, response) -> userService.update(request, response));
+		get("/user/", (request, response) -> userService.getAll(request, response));
+		get("/user/:id", (request, response) -> userService.get(request, response));
+		get("/user/:id", (request, response) -> userService.remove(request, response));
+		
+		post("/login/", (request, response) -> loginService.tryLogin(request, response));
+		
+		post("/eventos/", (request, response) -> eventService.add(request, response));
+		post("/eventos/", (request, response) -> eventService.update(request, response));
+		get("/eventos/", (request, response) -> eventService.getAll(request, response));
+		get("/eventos/:id", (request, response) -> eventService.get(request, response));
+		get("/eventos/:id", (request, response) -> eventService.remove(request, response));
 	}
 }
