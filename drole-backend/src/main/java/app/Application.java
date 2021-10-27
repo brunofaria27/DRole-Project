d6944fc458dbfb890d35576fdad7f53295c4e65d
Guilherme Dantas <guilherme.dantas1508@gmail.com>
2021-10-27 11:33:06 -0300

    Update application

--- drole-backend/src/main/java/app/Application.java
+++ drole-backend/src/main/java/app/Application.java
@@ -7,10 +7,8 @@ import services.LoginService;
 import services.UserService;
 
 public class Application {
-	private static Events eventTest = new Events(1, "Baile do teste", 3, "Pop", 18, 5, "Pendente",
-			"2022-11-11 00:00:00");
 	private static UserService userService = new UserService();
-	public static LoginService loginService = new LoginService();
+	private static LoginService loginService = new LoginService();
 	
 
 	public static void main(String[] args) {
@@ -33,24 +31,14 @@ public class Application {
 		});
 
 		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-		
-		get("/eventos", (request, response) -> eventTest.toString());
-		System.out.println(eventTest.toString());
-		
-		post("/hello", (request, response) -> methodString(request.body()));
-		
+	
 
-		post("/user/create", (request, response) -> userService.add(request, response)); // Avaliar método
+		post("/user/create", (request, response) -> userService.add(request, response));
 		post("/user/", (request, response) -> userService.update(request, response));
 		get("/user/", (request, response) -> userService.getAll(request, response));
 		get("/user/:id", (request, response) -> userService.get(request, response));
 		get("/user/remove/:id", (request, response) -> userService.remove(request, response));
 
 		get("/login/", (request, response) -> loginService.tryLogin(request, response)); // Avaliar método
-	}
-	
-	private static Object methodString(String string) {
-		System.out.println(string);
-		return null;
 	}
 }
