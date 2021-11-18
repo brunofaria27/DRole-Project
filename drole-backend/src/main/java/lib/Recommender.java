97ab5696dca89e1d889e1e4fdc6b1f880b09f690
Bruno Faria <bruno.fariadevl@gmail.com>
2021-11-18 12:19:46 -0300

    comentarios para acessar testes

--- drole-backend/src/main/java/lib/Recommender.java
+++ drole-backend/src/main/java/lib/Recommender.java
@@ -1,5 +1,10 @@
 package lib;
 
+/* IMPORTANTE
+ * https://github.com/danielSbastos/vibe-fi
+ * https://github.com/icei-pucminas/Sistemas-Inteligentes/blob/main/Azure_Classificacao_Java/Main.java
+ */
+
 public class Recommender {
 
 }
